6d0dd3024e0a88827fc7bd4b4846a1f09044f4c6
Gustav L <gurra.gl97@gmail.com>
2022-03-13 12:59:26 +0100

    continnued with the tour, also updated readme

--- README.md
+++ README.md
@@ -42,3 +42,72 @@ And just like before, we can assign values to theses
 > var a,b,c float64 = 1,24,5
 
 <hr>
+**Loops**
+there is only the *for* loop in go
+
+for is implemented just like "normal"...
+
+while is implemented like this
+> for CONDITION {STUFF}
+
+A forever loop is even easier than "normal"
+> for {STUFF}
+<hr>
+**IF**
+Little differances here... but some nice functionality is added.
+
+firstly, statement look
+> if CONDIFTION {STUFF}
+no parenthesis needed.
+
+The added functionallity:
+we can define variables in the if statement!
+if a:= 5 < 10{STUFF}
+the variable "a" is valid inside the if-scope
+
+we can have a function which might return an error
+>fun(input)(error)
+Put it inside the if statement and check if the value is not nil (meaning there was an error, and handle it inside the if statement
+
+if a:=fun(input);a != nil{
+    //handle the error
+    a contains the error
+}
+
+
+<hr>
+**Switches**
+note: im not a huge fan of switches, donno why
+
+Seems pretty stright forward.
+Only evaluates the first matching switch!
+Statement is defined in the following way:
+switch VARAIABLE {
+    case VALUE:
+        STUFF
+    case VALUE:
+        STUFF
+    default:
+        STUFF
+}
+
+one neat functionallity is that VARAIABLE is optional, meaning we can chain long if-else in a tidy way.
+
+[from tour](https://go.dev/tour/flowcontrol/11)
+t := time.Now()
+switch {
+case t.Hour() < 12:
+    fmt.Println("Good morning!")
+case t.Hour() < 17:
+    fmt.Println("Good afternoon.")
+default:
+    fmt.Println("Good evening.")
+}
+
+<hr>
+**DEFER**
+no idea what this is, is this somehting new and unique to go?
+Its like a function wrapper inside a function? the statement is executed after the rest of the function have returned... Sounds very odd
+
+we can have multiple defers in a single function, and they are executed in last-in-first-out manner, after the surounding functino is done.
+
--- goTour/main.go
+++ goTour/main.go
@@ -1,33 +1,98 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"time"
 )
-func simpleAdd(x int, y int) int{
-	return x+y
+
+func simpleAdd(x int, y int) int {
+	return x + y
 }
-func twoReturn(x string) (string, string){
-	return x,x
+func twoReturn(x string) (string, string) {
+	return x, x
 }
 
-func main(){
+func variableTests() {
 	fmt.Println("Hello world!", time.Now())
 	a := 1
 	b := 1336
-	fmt.Println("now i add two values", simpleAdd(a,b))
+	fmt.Println("now i add two values", simpleAdd(a, b))
 	s := "hello"
 	var ost string
-	_ , ost = twoReturn(s)
+	_, ost = twoReturn(s)
 	fmt.Println("here i use a function with two returns", ost)
 
 	v := 42 // change me!
 	fmt.Printf("v is of type %T\n", uint16(v))
 	v_2 := float64(v)
-	fmt.Printf("%T\n",v_2)
+	fmt.Printf("%T\n", v_2)
 
-	var g,j,c float64 = 1,24,5
+	var g, j, c float64 = 1, 24, 5
 
-	fmt.Printf("%T,%T,%T\n",g,j,c)
+	fmt.Printf("%T,%T,%T\n", g, j, c)
+}
 
+func forLoopTests() {
+	fmt.Println("==========LOOP LEARNING=============================")
+	// sum 10 numbers
+	x := 0
+	for i := 0; i < 10; i++ {
+		x += 1
+	}
+	fmt.Printf("after loop, x is %d\n", x)
+
+	// while loop is defined with the first and last "for"-arguments ommited
+	var gurk, ost int = 6, 0
+	for gurk < 278 {
+		gurk *= 2
+		ost += 1
+	}
+	fmt.Printf("we reached number %d, we needed  %d loops \n", gurk, ost)
+
+	// a better implementation perhaps idk
+	gurk = 6
+	for ost = 0; gurk < 278; ost++ {
+		gurk *= 2
+	}
+	fmt.Printf("we reached number %d, we needed  %d loops \n", gurk, ost)
+
+}
+
+func ifTests() error {
+	a, b := 5, 20
+
+	if a < b {
+		fmt.Printf("Yes, a is less than b, %d,%d\n", a, b)
+	} else {
+		return errors.New("this is very odd")
+	}
+
+	if g := 5; g < 20 {
+		fmt.Printf("you can apparently create variables in the if-statement itself\n")
+	}
+
+	//just a quick test
+	h := "a"
+	fmt.Printf("the numerical value of \"a\" is %d\n", int(h[0]))
+	return nil
+}
+
+func switchTest() {
+	fmt.Printf("==============Switch======================\n")
+	gaga := "hello"
+	switch gaga {
+	case "hi":
+		fmt.Printf("The text is gaga\n")
+	case "hello":
+		fmt.Printf("The value is \"hello\"\n")
+	default:
+		fmt.Printf("there was no matching case...\n")
+	}
+}
+func main() {
+	variableTests()
+	forLoopTests()
+	ifTests()
+	switchTest()
 }
